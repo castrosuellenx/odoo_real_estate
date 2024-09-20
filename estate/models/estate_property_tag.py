0228dca488a9661198b0bc9aedbd370fd6f881ae
castrosuellenx <castrosuellenx@gmail.com>
2024-09-20 18:04:49 -0300

    add styles on models from estate addon

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = 'name'
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [
         ('check_unique_tag_name', 'UNIQUE(name)', 'The name must be unique.'),
