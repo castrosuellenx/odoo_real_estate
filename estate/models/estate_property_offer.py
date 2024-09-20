761b917b1f1be390b068c9864364d236b05c078f
castrosuellenx <castrosuellenx@gmail.com>
2024-09-20 14:28:15 -0300

    update models from estate addon to have deterministic order

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
+    _order = "price desc"
 
     price = fields.Float()
     status = fields.Selection(
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,8 +3,10 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = 'Real Estate Property Type'
+    _order = 'sequence, name'
 
     name = fields.Char(required=True)
+    sequence = fields.Integer('Sequence', default=1, help='Used to order property types. Lower means more used.')
     property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
