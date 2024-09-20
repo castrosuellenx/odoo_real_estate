06f7d62e19dbea95629ec0b5acaa9ff9799835cb
castrosuellenx <castrosuellenx@gmail.com>
2024-09-20 18:50:23 -0300

    add offers stat button to estate_property_type model

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -8,7 +8,23 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     sequence = fields.Integer('Sequence', default=1, help='Used to order property types. Lower means more used.')
     property_ids = fields.One2many('estate.property', 'property_type_id')
+    offer_count = fields.Integer(compute='_compute_offer_count', string="Offers")
 
     _sql_constraints = [
         ('check_unique_type_name', 'UNIQUE(name)', 'The name must be unique.'),
     ]
+
+    @api.depends('property_ids.offer_ids')
+    def _compute_offer_count(self):
+        for property_type in self:
+            property_type.offer_count = len(property_type.property_ids.offer_ids)
+
+    def action_view_offers(self):
+        self.ensure_one()
+        return {
+            'name': 'Property Offers',
+            'view_mode': 'tree,form',
+            'res_model': 'estate.property.offer',
+            'type': 'ir.actions.act_window',
+            'domain': [('property_id.property_type_id', '=', self.id)],
+        }
